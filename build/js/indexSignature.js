54b8761ce1cf3c0a7e10002ab8f79b3bd992c380
Prashant <prashant.p@simformsolutions.com>
2023-03-20 17:11:33 +0530

    keyof keyword

--- build/js/indexSignature.js
+++ build/js/indexSignature.js
@@ -26,4 +26,40 @@ const todaysNet = (transactions) => {
     return total;
 };
 console.log(todaysNet(todaysTransactions));
-log;
+console.log("Keyof keyword");
+const student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+};
+// console.log(student.test);
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+const keyofstudent = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+};
+// console.log(student.test);
+for (const key in keyofstudent) {
+    console.log(`${keyofstudent[key]}`);
+}
+// When you don't know the interface or type of object
+Object.keys(keyofstudent).map(key => {
+    console.log(`hello ${keyofstudent[key]}`);
+});
+const logStudentket = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentket(student, 'name');
+logStudentket(student, 'GPA');
+logStudentket(student, 'classes');
+const monthlyIncome = {
+    salary: 50000,
+    bonus: 10000,
+    sidehustle: 100000
+};
+for (const revenue in monthlyIncome) {
+    console.log(monthlyIncome[revenue]);
+}
--- src/indexSignature.ts
+++ src/indexSignature.ts
@@ -8,7 +8,7 @@ console.log('Index Signature');
 // property dynamically.
 
 interface TransactionObj {
-    [index:string]:number 
+    [index: string]: number
     //index cannot be boolean
 }
 // interface TransactionObj {
@@ -45,5 +45,77 @@ const todaysNet = (transactions: TransactionObj):
 
 console.log(todaysNet(todaysTransactions));
 
-log
+console.log("Keyof keyword");
+
+
+interface Student {
+    [key: string]: string | number | number[] | undefined
+    name: string,
+    GPA: number,
+    classes?: number[]
+}
+
+const student: Student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+}
+
+// console.log(student.test);
+
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+
+interface keyofStudent {
+    // [key: string]:  string| number |number[]|undefined
+    name: string,
+    GPA: number,
+    classes?: number[]
+}
+
+const keyofstudent: keyofStudent = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+}
+
+// console.log(student.test);
+
+for (const key in keyofstudent) {
+    console.log(`${keyofstudent[key as keyof keyofStudent]}`);
+}
+// When you don't know the interface or type of object
+Object.keys(keyofstudent).map(key => {
+    console.log(`hello ${keyofstudent[key as keyof typeof keyofstudent]}`);
+})
+
+
+const logStudentket = (student: Student, key: keyof Student): void => {
+    console.log(`Student ${key}: ${student[key]}`);
+
+}
+
+logStudentket(student, 'name')
+logStudentket(student, 'GPA')
+logStudentket(student, 'classes')
+
+// interface Streams {
+//     [key: string]:number
+// }
+type Streams = 'salary' | 'bonus' | 'sidehustle'
+
+type incomes = Record<Streams, number>
+
+const monthlyIncome: incomes = {
+    salary: 50000,
+    bonus: 10000,
+    sidehustle: 100000
+}
+
+for (const revenue in monthlyIncome) {
+    console.log(monthlyIncome[revenue as keyof incomes]);
+}
+
+
 
